ba0f9d1f31ad0d14c805bcda35dc5403e1ae40a9
Arn Vanhoutte <arn@livios.onmicrosoft.com>
2023-12-04 10:36:02 +0100

    Initial setup

---
+++ editor.js
@@ -0,0 +1,23 @@
+﻿ClassicEditor
+    .create(document.querySelector('.editor'), {
+        toolbar: ['undo', 'redo',
+            '|', 'bold', 'italic', 'subscript', 'superscript',
+            '|', 'outdent', 'indent', 'bulletedList', 'numberedList',
+            '|', 'link', 'mediaEmbed', 'specialcharacters', 'pastefromoffice'],
+        mediaEmbed: { previewsInData: true },
+        language: "nl-BE"
+    })
+	.then( editor => {
+		window.editor = editor;
+	} )
+    .catch((error) => {
+        const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+        const message = [
+            'Oops, something went wrong!',
+            `Please, report the following error on ${issueUrl} with the build id "y20w2tflmd55-do5q4gutap8d" and the error stack trace:`
+        ].join('\n');
+
+        console.error(message);
+        console.error(error);
+    });

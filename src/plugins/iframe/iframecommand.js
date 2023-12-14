4baf9cdcdd0f8a3a3ba198c33e9f90f19eff992a
Arn Vanhoutte <arn.vanhoutte@persgroep.net>
2023-12-14 14:44:39 +0100

    Code cleanup

--- src/plugins/iframe/iframecommand.js
+++ src/plugins/iframe/iframecommand.js
@@ -5,7 +5,7 @@ export default class IframeCommand extends Command {
     refresh() {
         const model = this.editor.model;
         const selection = model.document.selection.getFirstPosition().parent;
-        console.log(selection)
+        
         this.isEnabled = true;
         this.value = selection.name == "iframe";
     }
@@ -13,14 +13,14 @@ export default class IframeCommand extends Command {
     execute({src, width, height}) {
         const model = this.editor.model;
         var style = 'width: '+width+'; height: '+height;
-        console.info(src, width, height, style)
+        
         model.change(writer => {
             const iframeElement = writer.createElement('iframe', 
             { 
                 src: src, 
                 style: style,
-                width: width,
-                height: height
+                width: width + 'px',
+                height: height + 'px'
             });
 			
             model.insertContent(iframeElement );
